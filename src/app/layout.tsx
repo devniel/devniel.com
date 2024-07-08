d3837c01d25bb468b921d92ea41309e5af3bfba4
Daniel Mauricio Flores <dnielfs@gmail.com>
2024-07-08 15:16:34 +0200

    feat: add analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import { type Metadata } from 'next'
 
 import { Providers } from '@/app/providers'
 import { Layout } from '@/components/Layout'
+import { Analytics } from '@vercel/analytics/react';
 
 import '@/styles/tailwind.css'
 
@@ -33,6 +34,7 @@ export default function RootLayout({
             <Layout>{children}</Layout>
           </div>
         </Providers>
+        <Analytics />
       </body>
     </html>
   )
